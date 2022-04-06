cb12fb243eae50d110d859171ef9d069c8f6b2f5
Eva <rutheve.eva@gmail.com>
2022-04-06 18:19:41 +0300

    path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './UserComponents/headerComponent/header.component';
 import {FooterComponent} from './UserComponents/footerComponent/footer.component';
-import { EventBindingComponent } from './app/UserComponents/Basics/Binding/event-binding/event-binding.component';
+
+import { EventBindingComponent } from './UserComponents/Binding/event-binding/event-binding.component';
 @NgModule({
   declarations: [
     AppComponent,
